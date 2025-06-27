bdd1d5b132b6891852aa1937c97d27c4f1a68141
AdelFC <AdelFC@users.noreply.github.com>
2025-06-27 14:29:51 +0200

    routine OK

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: afodil-c <afodil-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 18:20:41 by afodil-c          #+#    #+#             */
-/*   Updated: 2025/06/24 11:04:12 by afodil-c         ###   ########.fr       */
+/*   Updated: 2025/06/27 14:27:36 by afodil-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,56 +22,21 @@
  *   - Fonction de thread qui boucle tant que la simulation est active.
  *   - Retourne NULL à la fin de la simulation.
  */
-static void	philo_take_forks_odd(t_philo *philo)
-{
-	if (philo->philo_id % 2 == 0)
-	{
-		lock_or_exit(&philo->left_fork->fork);
-		ft_print_status(TAKE_FIRST_FORK, philo);
-		lock_or_exit(&philo->right_fork->fork);
-		ft_print_status(TAKE_SECOND_FORK, philo);
-	}
-	else
-	{
-		lock_or_exit(&philo->right_fork->fork);
-		ft_print_status(TAKE_FIRST_FORK, philo);
-		lock_or_exit(&philo->left_fork->fork);
-		ft_print_status(TAKE_SECOND_FORK, philo);
-	}
-}
-
-static void	philo_take_forks_even(t_philo *philo)
-{
-	if (philo->left_fork->fork_id < philo->right_fork->fork_id)
-	{
-		lock_or_exit(&philo->left_fork->fork);
-		ft_print_status(TAKE_FIRST_FORK, philo);
-		lock_or_exit(&philo->right_fork->fork);
-		ft_print_status(TAKE_SECOND_FORK, philo);
-	}
-	else
-	{
-		lock_or_exit(&philo->right_fork->fork);
-		ft_print_status(TAKE_FIRST_FORK, philo);
-		lock_or_exit(&philo->left_fork->fork);
-		ft_print_status(TAKE_SECOND_FORK, philo);
-	}
-}
-
 static void	philo_take_forks(t_philo *philo)
 {
 	t_table	*table;
 
 	table = philo->table;
-	if (table->philo_nbr % 2 != 0)
-		philo_take_forks_odd(philo);
-	else
-		philo_take_forks_even(philo);
+	lock_or_exit(&philo->right_fork->fork);
+	ft_print_status(TAKE_FIRST_FORK, philo);
+	lock_or_exit(&philo->left_fork->fork);
+	ft_print_status(TAKE_SECOND_FORK, philo);
 }
 
 static void	philo_eat_sleep_think(t_philo *philo)
 {
-	wait_for_start(philo->table);
+	int	time_to_think;
+
 	philo_take_forks(philo);
 	lock_or_exit(&philo->philo_mtx);
 	philo->last_meal_time = ft_time(MILLISECOND);
@@ -87,6 +52,11 @@ static void	philo_eat_sleep_think(t_philo *philo)
 	ft_print_status(SLEEP, philo);
 	ft_usleep(philo->table->time_to_sleep * 1000, philo->table);
 	ft_print_status(THINK, philo);
+	time_to_think = philo->table->time_to_die - philo->table->time_to_eat
+		- philo->table->time_to_sleep - 100;
+	if (time_to_think < 0)
+		time_to_think = 1;
+	ft_usleep(time_to_think * 1000, philo->table);
 }
 
 void	*philo_routine(void *arg)
@@ -94,8 +64,12 @@ void	*philo_routine(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	wait_for_start(philo->table);
 	if (philo->philo_id % 2 == 0)
-		ft_usleep(1000, philo->table);
+	{
+		ft_print_status(THINK, philo);
+		ft_usleep((philo->table->time_to_eat / 2) * 1000, philo->table);
+	}
 	while (!get_end_simulation(philo->table))
 		philo_eat_sleep_think(philo);
 	return (NULL);
